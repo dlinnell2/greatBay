daf48dfe57e5d9262da58b422ee2caba553e0eff
Donald Linnell <dlinnell@Donalds-MacBook-Air.local>
2018-04-14 13:36:04 -0500

    Selecting an item and storing it to the local machine

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var newBid;
+var currentBid;
+var currentItem;
+var choices = [];
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -10,18 +13,49 @@ var connection = mysql.createConnection({
     database: 'greatBayDB'
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log(`Connected as id ${connection.threadId}`);
 });
 
-connection.query('SELECT * FROM', function(err, res){
-    if (err) console.log (err);
+function displayBidItems() {
 
-    for (item of res){
-        
-    }
-})
+    connection.query('SELECT * FROM items', function (err, res) {
+        if (err) console.log(err);
+
+        for (item of res) {
+            choices.push(item.item);
+        }
+
+        selectItem();
+
+    });
+
+}
+
+function selectItem(){
+
+    inquirer.prompt([
+        {
+        type: 'list',
+        name: 'choosenItem',
+        message: 'Please choose which item you would like to bid on',
+        choices: choices
+        }
+    ]).then(function(item){
+        currentItem = item.choosenItem;
+
+        connection.query('SELECT * FROM items WHERE ?', {
+            item: currentItem
+        }, function(err, res){
+            currentBid = res[0].price;
+            
+        });
+
+    });
+
+};
+
 function postItems() {
 
     inquirer.prompt([
@@ -35,13 +69,15 @@ function postItems() {
             name: "price",
             message: "What is this item's price?"
         }
-    ]).then(function(item) {
+    ]).then(function (item) {
         if (item.itemName) {
             console.log("Your item is " + item.itemName);
-            console.log("Your item costs: "+ item.price);
+            console.log("Your item costs: " + item.price);
         } else {
             console.log("Goodbye");
             return;
         }
     })
 }
+
+displayBidItems();
