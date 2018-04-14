7efdb254b792482dd577e4d19e2a250d818dec81
Donald Linnell <dlinnell@Donalds-MacBook-Air.local>
2018-04-14 13:06:18 -0500

    Seed file for MySQL

---
+++ greatBaySeed.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS greatBayDB;
+
+CREATE DATABASE greatBayDB;
+
+USE greatBayDB;
+
+CREATE TABLE items (
+
+  id INT NOT NULL AUTO_INCREMENT,
+  item VARCHAR(50) NOT NULL,
+  price INTEGER(10) NOT NULL,
+  PRIMARY KEY (id)
+
+);
+
+INSERT INTO items (item, price)
+VALUES ('Record Player', 250);
+
+INSERT INTO items (item, price)
+VALUES ('Electric Guitar', 135);
+
+INSERT INTO items (item, price)
+VALUES ('Bicycle', 200);
+
+INSERT INTO items (item, price)
+VALUES ('Honda Accord', 2000);
+
+INSERT INTO items (item, price)
+VALUES ('Laptop', 450);
+
+INSERT INTO items (item, price)
+VALUES ('Empty Starbucks Cup', 5);
+
+INSERT INTO items (item, price)
+VALUES ('Full Starbucks Cup', 3);
+
+INSERT INTO items (item, price)
+VALUES ('Vintage Converse', 45);
+
+INSERT INTO items (item, price)
+VALUES ('A bag of steel cut oats', 30);
+
+INSERT INTO items (item, price)
+VALUES ('Helicopter', 3000);
