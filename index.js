f13503263718f5b538bd7f1d498c7cf703db3ae8
Yuta <yutaatamura@gmail.com>
2018-04-14 13:10:47 -0500

    New item function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -11,3 +12,27 @@ connection.connect(function(err){
     if (err) throw err;
     console.log(`Connected as id ${connection.threadId}`);
 });
+
+function postItems() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemName",
+            message: "What is your item?"
+        },
+        {
+            type: "input",
+            name: "price",
+            message: "What is this item's price?"
+        }
+    ]).then(function(item) {
+        if (item.itemName) {
+            console.log("Your item is " + item.itemName);
+            console.log("Your item costs: "+ item.price);
+        } else {
+            console.log("Goodbye");
+            return;
+        }
+    })
+}
